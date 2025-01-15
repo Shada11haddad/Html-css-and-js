91bf6ab47c71bd64ac69af0a7f2a155a4ab775ca
shada <103006276+shadaESQ@users.noreply.github.com>
2025-01-15 16:27:06 +0300

    this is an update

---
+++ JScouser/Basics/app.js
@@ -0,0 +1,62 @@
+// Variables
+// Reserved keywords  - var, let, const
+// variable name - camelCase
+// var name = 'John Doe';
+//teyp of variable
+
+const name = "shada";
+
+let whatDoYouWannaBecomeIYourLife;
+whatDoYouWannaBecomeIYourLife = "prgogrammer";
+const gender ="Female";
+const twitterHandle = "shada_019";
+//-----------------------------//
+let firstFavNumber = 5;
+let secondFavNumber = 10;
+let sum = firstFavNumber + secondFavNumber;
+let difference = firstFavNumber - secondFavNumber;
+let product = firstFavNumber * secondFavNumber;
+let quotient = firstFavNumber / secondFavNumber;
+let remainder = firstFavNumber % secondFavNumber;
+let power = firstFavNumber ** secondFavNumber;
+console.log(sum);
+console.log(difference);
+console.log(product);   
+console.log(quotient);
+console.log(remainder);
+console.log(power);
+//-----------------------------//
+
+const isJsProgrammingLanguage = true;
+const isJsHard = false;
+const favNumb = 4;
+console.log(favNumb + undefined)
+//-----------------------------//
+
+console.log("Is firstFavNumber greater than secondFavNumber: " + (firstFavNumber > secondFavNumber));
+console.log("Is firstFavNumber less than secondFavNumber: " + (firstFavNumber < secondFavNumber));
+console.log("Is firstFavNumber greater than or equal to secondFavNumber: " + (firstFavNumber >= secondFavNumber));
+console.log("Is firstFavNumber less than or equal to secondFavNumber: " + (firstFavNumber <= secondFavNumber));
+console.log("Is firstFavNumber loosely equal to secondFavNumber: " + (firstFavNumber == secondFavNumber));  // loose equality
+console.log("Is firstFavNumber loosely not equal to secondFavNumber: " + (firstFavNumber != secondFavNumber));  // loose inequality
+console.log("Is firstFavNumber strictly equal to secondFavNumber: " + (firstFavNumber === secondFavNumber)); // strict equality
+console.log("Is firstFavNumber strictly not equal to secondFavNumber: " + (firstFavNumber !== secondFavNumber)); // strict inequality
+//------------Manipulating String-----------------//
+let favActorFirstName = "Tom";
+let favActorLasttName = "Hanks";
+let fullActorName = favActorFirstName + " " + favActorLasttName;
+let UPPERCASE = fullActorName.toUpperCase();
+let massege = "Hello " + fullActorName + "!";
+let massege1 = `Hello ${fullActorName}!`;
+// append
+let favMovie = "Forest Gump";
+let favMovieQuote = "Life is like a box of chocolates";
+let favMovieQuote1 = `Life is like a box of chocolates`;
+favMovieQuote1 += `Life is like a box of chocolates. ${favMovie}`;
+let favMovieQuote3 = `Life is like a box of chocolates. ${favMovie} ${favActorFirstName} ${favActorLasttName}`;
+//------------Manipulating Array-----------------//
+//-----------Type Conversion------------------//
+
+//-----------Conditional Statements------------------//
+
+//-----------Switch Statements------------------//
---
+++ JSgame/LearnJS/main.js
@@ -0,0 +1,6 @@
+const name = Null;
+console.log(Four); // 4
+f
+// Four = 5;
+
+console.log(Four);
