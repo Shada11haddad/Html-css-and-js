efe991d790601970c7362c31134a0676d205ce9b
shada <103006276+shadaESQ@users.noreply.github.com>
2025-02-25 20:51:09 +0300

    ncks

--- fullstack/ProductStore/BackEnd/server.js
+++ fullstack/ProductStore/BackEnd/server.js
@@ -10,11 +10,16 @@ app.get("/", (req, res) => {
 app.post("/products", async(req, res) => {
     const products = req.body;
     res.send(products);
+    if(!products.name || !products.price || !products.image){
+        return res.status(400).json({ success:false, message: "please fill all the fields"});
+    }
+    const newProduct = new Product
 });
 
 console.log(process.env.PORT);
 const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
+    connectDB();
     console.log('Server started at http://localhost:5000');
 }); 
